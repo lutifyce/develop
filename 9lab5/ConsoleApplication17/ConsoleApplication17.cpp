fecee3e772ded0a64ad0fb4a4bf70b60bcd4d926
lutifyce <lutifyce@mail.ru>
2021-12-28 00:11:46 +0300

    9lab

---
+++ 9lab5/ConsoleApplication17/ConsoleApplication17.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int h, m, s;
+
+	cout<<"Hours:";
+	cin >> h;
+	cout<< "Minuts:";
+	cin >> m;
+	cout<< "Seconds";
+	cin>> s;
+	if (s > 59||m>59)
+	{
+		cout << "Error";
+		return(0);
+	}
+	cout<< "Time in seconds: "<<(h * 3600) + (m * 60) + s;
+}
